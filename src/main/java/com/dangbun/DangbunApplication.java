f4ca6d8d94a27b9f8d2cf43e649ba37de2f265cc
jiyun921 <ryn0921@naver.com>
2025-07-03 15:52:12 +0900

    init: dangbun project

---
+++ src/main/java/com/dangbun/DangbunApplication.java
@@ -0,0 +1,13 @@
+package com.dangbun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangbunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangbunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dangbun/DangbunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangbun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangbunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
