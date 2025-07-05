9236d7a8d5cdd0785a300c3c1dd57540d7e27b8f
jiyun921 <ryn0921@naver.com>
2025-07-05 19:44:47 +0900

    [test] : 배포 ci/cd 최종 확인

--- src/main/java/com/dangbun/HomeController.java
+++ src/main/java/com/dangbun/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String func(){
-        return "hello world!";
+        return "hello world!!!!!!!";
     }
 
 }
