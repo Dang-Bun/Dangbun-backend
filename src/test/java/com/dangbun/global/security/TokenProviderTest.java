3c5c41588db1f501819f8a04f454f2ffc1654510
jonnykim1o28 <jonnykim1o28@gmail.com>
2025-07-17 15:54:00 +0900

    [fix]:Test 수정

--- src/test/java/com/dangbun/global/security/TokenProviderTest.java
+++ src/test/java/com/dangbun/global/security/TokenProviderTest.java
@@ -1,10 +1,7 @@
 package com.dangbun.global.security;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
 class TokenProviderTest {
 
 
