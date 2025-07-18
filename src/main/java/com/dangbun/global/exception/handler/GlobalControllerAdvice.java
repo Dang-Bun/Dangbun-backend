140f0000ea9911b8e34b74a7ff9c91622296f909
jiyun921 <ryn0921@naver.com>
2025-07-19 02:21:22 +0900

    [feat]: Global 예외 REQUIRED_FIELD_MISSING 추가

--- src/main/java/com/dangbun/global/exception/handler/GlobalControllerAdvice.java
+++ src/main/java/com/dangbun/global/exception/handler/GlobalControllerAdvice.java
@@ -3,6 +3,7 @@ package com.dangbun.global.exception.handler;
 
 import com.dangbun.global.exception.BadRequestException;
 import com.dangbun.global.response.BaseErrorResponse;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.TypeMismatchException;
 import org.springframework.core.Ordered;
@@ -24,7 +25,7 @@ public class GlobalControllerAdvice {
 
     // 잘못된 요청일 경우
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({BadRequestException.class, TypeMismatchException.class, MethodArgumentNotValidException.class, MissingServletRequestParameterException.class})
+    @ExceptionHandler({BadRequestException.class, TypeMismatchException.class, MissingServletRequestParameterException.class})
     public BaseErrorResponse handle_BadRequest(Exception e){
         log.error("[handle_BadRequest]", e);
         return new BaseErrorResponse(BAD_REQUEST);
@@ -44,5 +45,20 @@ public class GlobalControllerAdvice {
     public BaseErrorResponse handle_RuntimeException(Exception e) {
         log.error("[handle_RuntimeException]", e);
         return new BaseErrorResponse(INTERNAL_SERVER_ERROR);
+    }
+
+    // DTO validation 실패 (예: @NotBlank, @NotNull 등)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseErrorResponse handleValidationException(Exception e) {
+        log.error("[handle_ValidationException]", e);
+        return new BaseErrorResponse(REQUIRED_FIELD_MISSING);
+    }
+
+    // RequestParam, PathVariable 등의 validation 실패 (예: @RequestParam 제약 위반)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public BaseErrorResponse handleConstraintViolation(Exception e) {
+        return new BaseErrorResponse(REQUIRED_FIELD_MISSING);
     }
 }
