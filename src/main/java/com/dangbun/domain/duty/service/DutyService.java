0ba8e0d25b1f6f5b115463b7fe8319b2d235e1cf
jiyun921 <ryn0921@naver.com>
2025-07-19 02:24:06 +0900

    [feat]: DutyService - createDuty 구현

---
+++ src/main/java/com/dangbun/domain/duty/service/DutyService.java
@@ -0,0 +1,41 @@
+package com.dangbun.domain.duty.service;
+
+import com.dangbun.domain.duty.dto.request.PostDutyCreateRequest;
+import com.dangbun.domain.duty.dto.response.PostDutyCreateResponse;
+import com.dangbun.domain.duty.entity.Duty;
+import com.dangbun.domain.duty.exception.custom.*;
+import com.dangbun.domain.duty.repository.DutyRepository;
+import com.dangbun.domain.place.entity.Place;
+import com.dangbun.domain.place.repository.PlaceRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import static com.dangbun.domain.duty.response.status.DutyExceptionResponse.*;
+
+@Service
+@RequiredArgsConstructor
+public class DutyService {
+
+    private final DutyRepository dutyRepository;
+    private final PlaceRepository placeRepository;
+
+    @Transactional
+    public PostDutyCreateResponse createDuty(Long placeId, PostDutyCreateRequest request) {
+        Place place = placeRepository.findById(placeId)
+                .orElseThrow(() -> new PlaceNotFoundException(PLACE_NOT_FOUND));
+
+        if (dutyRepository.existsByNameAndPlace_PlaceId(request.name(), placeId)) {
+            throw new DutyAlreadyExistsException(DUTY_ALREADY_EXISTS);
+        }
+
+        Duty duty = Duty.builder()
+                .name(request.name())
+                .icon(request.icon())
+                .place(place)
+                .build();
+
+        Duty saved = dutyRepository.save(duty);
+        return PostDutyCreateResponse.of(saved.getDutyId());
+    }
+
+}
