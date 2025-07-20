fa21ae0cdce1fbfdbb28cb77c197f99e0e3c6650
jiyun921 <ryn0921@naver.com>
2025-07-21 04:21:40 +0900

    [feat]: DutyRepository - findByPlace_PlaceId 구현

--- src/main/java/com/dangbun/domain/duty/repository/DutyRepository.java
+++ src/main/java/com/dangbun/domain/duty/repository/DutyRepository.java
@@ -3,6 +3,10 @@ package com.dangbun.domain.duty.repository;
 import com.dangbun.domain.duty.entity.Duty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DutyRepository extends JpaRepository<Duty, Long> {
     boolean existsByNameAndPlace_PlaceId(String name, Long placeId);
+    List<Duty> findByPlace_PlaceId(Long placeId);
+
 }
