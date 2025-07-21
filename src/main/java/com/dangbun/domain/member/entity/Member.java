716db4734eb36930de53e5dba09fb9e8eec8c3bd
jiyun921 <ryn0921@naver.com>
2025-07-21 15:07:14 +0900

    [feat] : Member, MemberRole 구현

--- src/main/java/com/dangbun/domain/member/entity/Member.java
+++ src/main/java/com/dangbun/domain/member/entity/Member.java
@@ -1,4 +1,48 @@
 package com.dangbun.domain.member.entity;
 
+import com.dangbun.domain.duty.entity.DutyIcon;
+import com.dangbun.domain.place.entity.Place;
+import com.dangbun.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    @Column(nullable = false)
+    private Boolean status;
+
+    @Column(columnDefinition = "json")
+    private String information;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Member(MemberRole role, Boolean status, String information, Place place, User user) {
+       this.role = role;
+       this.status = status;
+       this.information = information;
+       this.place = place;
+       this.user = user;
+    }
+
 }
