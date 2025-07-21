997a155d533b69228ef7511dd31fee2c2745c81f
jiyun921 <ryn0921@naver.com>
2025-07-21 15:06:54 +0900

    [feat] : Cleaning RepeatType 구현

---
+++ src/main/java/com/dangbun/domain/cleaning/entity/RepeatType.java
@@ -0,0 +1,17 @@
+package com.dangbun.domain.cleaning.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+enum RepeatType {
+    DAILY("매일"),
+    WEEKLY("매주 요일마다"),
+    MONTHLY_FIRST("매달 첫 날"),
+    MONTHLY_LAST("매달 마지막 날"),
+    NONE("없음")
+    ;
+
+    private final String displayName;
+}
