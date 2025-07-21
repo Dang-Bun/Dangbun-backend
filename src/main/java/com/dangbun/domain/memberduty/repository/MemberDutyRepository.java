2b8cf06f8866154510d21ad2134bcf9a348502db
jiyun921 <ryn0921@naver.com>
2025-07-21 19:39:50 +0900

    [feat] : MemberRepository, MemberDutyRepository - existsByDutyAndMember 구현

--- src/main/java/com/dangbun/domain/memberduty/repository/MemberDutyRepository.java
+++ src/main/java/com/dangbun/domain/memberduty/repository/MemberDutyRepository.java
@@ -1,6 +1,7 @@
 package com.dangbun.domain.memberduty.repository;
 
 import com.dangbun.domain.duty.entity.Duty;
+import com.dangbun.domain.member.entity.Member;
 import com.dangbun.domain.memberduty.entity.MemberDuty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 public interface MemberDutyRepository extends JpaRepository<MemberDuty, Long> {
 
     List<MemberDuty> findAllByDuty(Duty duty);
+    boolean existsByDutyAndMember(Duty duty, Member member);
+
 }
