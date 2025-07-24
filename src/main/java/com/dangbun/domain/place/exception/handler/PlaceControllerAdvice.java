80447eb9cbf9dac18ae2dd59a933ff38705ecbac
김종혁 <jonnykim1o28@gmail.com>
2025-07-24 21:57:22 +0900

    [fix]: ControllerAdvice->RestControllerAdvice

--- src/main/java/com/dangbun/domain/place/exception/handler/PlaceControllerAdvice.java
+++ src/main/java/com/dangbun/domain/place/exception/handler/PlaceControllerAdvice.java
@@ -3,15 +3,15 @@ package com.dangbun.domain.place.exception.handler;
 import com.dangbun.domain.place.exception.custom.InvalidInviteCodeException;
 import com.dangbun.global.response.BaseErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class PlaceControllerAdvice {
 
     @ExceptionHandler(InvalidInviteCodeException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public BaseErrorResponse handleInvalidRoleException(InvalidInviteCodeException e) {
         return new BaseErrorResponse(e.getStatus());
     }
