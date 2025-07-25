1e7ba152ae57375613adde79e2be791f4257ef11
jiyun921 <ryn0921@naver.com>
2025-07-25 18:23:21 +0900

    [feat] : CleaningRepository - findByDutyIdAndMemberIds, MemberCleaningRepository - findMembersByCleaningId 구현

--- src/main/java/com/dangbun/domain/cleaning/repository/CleaningRepository.java
+++ src/main/java/com/dangbun/domain/cleaning/repository/CleaningRepository.java
@@ -3,9 +3,17 @@ package com.dangbun.domain.cleaning.repository;
 import com.dangbun.domain.cleaning.entity.Cleaning;
 import com.dangbun.domain.duty.entity.Duty;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CleaningRepository extends JpaRepository<Cleaning, Long> {
     List<Cleaning> findAllByDuty(Duty duty);
+
+    @Query("""
+    SELECT c FROM Cleaning c
+    JOIN MemberCleaning mc ON c.cleaningId = mc.cleaning.cleaningId
+    WHERE c.duty.dutyId = :dutyId AND mc.member.memberId IN :memberIds
+    """)
+    List<Cleaning> findByDutyIdAndMemberIds(Long dutyId, List<Long> memberIds);
 }
