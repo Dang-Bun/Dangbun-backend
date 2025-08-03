ae9bb571a1ce06ff349bbbac66accc9a635241a0
jiyun921 <ryn0921@naver.com>
2025-08-03 22:01:32 +0900

    [feat] NotificationController 구현

--- src/main/java/com/dangbun/domain/notification/controller/NotificationController.java
+++ src/main/java/com/dangbun/domain/notification/controller/NotificationController.java
@@ -2,10 +2,12 @@ package com.dangbun.domain.notification.controller;
 
 import com.dangbun.domain.notification.dto.request.PostNotificationCreateRequest;
 import com.dangbun.domain.notification.dto.response.GetMemberSearchListResponse;
+import com.dangbun.domain.notification.dto.response.GetNotificationListResponse;
 import com.dangbun.domain.notification.dto.response.GetRecentSearchResponse;
 import com.dangbun.domain.notification.dto.response.PostNotificationCreateResponse;
 import com.dangbun.domain.notification.response.status.NotificationExceptionResponse;
 import com.dangbun.domain.notification.service.NotificationService;
+import com.dangbun.domain.notificationreceiver.dto.response.GetNotificationReceivedListResponse;
 import com.dangbun.global.CheckPlaceMembership;
 import com.dangbun.global.docs.DocumentedApiErrors;
 import com.dangbun.global.response.BaseResponse;
@@ -16,6 +18,8 @@ import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
@@ -43,7 +47,7 @@ public class NotificationController {
             @RequestParam(required = false) @Size(max = 20) String searchname,
             @ParameterObject Pageable pageable
     ) {
-        return ResponseEntity.ok(BaseResponse.ok(notificationService.searchMembers(placeId, searchname, pageable)));
+        return ResponseEntity.ok(BaseResponse.ok(notificationService.searchMembers(searchname, pageable)));
     }
 
     @Operation(summary = "알림함 - 수신인(멤버 검색) 최근 검색어 조회", description = "수신인 선택 화면에서 최근 검색한 멤버 이름을 최대 5개까지 조회합니다.")
@@ -54,7 +58,7 @@ public class NotificationController {
     )
     public ResponseEntity<BaseResponse<GetRecentSearchResponse>> getRecentSearches(
             @RequestParam Long placeId) {
-        return ResponseEntity.ok(BaseResponse.ok(notificationService.getRecentSearches(placeId)));
+        return ResponseEntity.ok(BaseResponse.ok(notificationService.getRecentSearches()));
     }
 
     @Operation(summary = "알림함 - 알림 작성", description = "작성한 내용의 알림을 전송합니다.")
@@ -69,6 +73,21 @@ public class NotificationController {
     ) {
 
         return ResponseEntity.ok(BaseResponse.ok(notificationService.createNotification(request)));
+    }
+
+    @Operation(summary = "보낸 알림 목록 조회 (무한스크롤)", description = "현재 로그인한 멤버가 보낸 알림들을 무한스크롤 방식으로 조회합니다.")
+    @GetMapping("/sent")
+    @DocumentedApiErrors(
+            value = {NotificationExceptionResponse.class},
+            includes = {""}
+    )
+    public ResponseEntity<BaseResponse<GetNotificationListResponse>> getNotifications(
+            @RequestParam Long placeId,
+            @ParameterObject
+            @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC)
+            Pageable pageable
+    ) {
+        return ResponseEntity.ok(BaseResponse.ok(notificationService.getNotificationList(pageable)));
     }
 
 }
