bf95e9b77fbd8e44ece15ad57feb4191cf42f2f9
jiyun921 <ryn0921@naver.com>
2025-08-03 21:08:54 +0900

    [feat] NotificationReceiverController- getReceivedNotifications 구현

---
+++ src/main/java/com/dangbun/domain/notificationreceiver/controller/NotificationReceiverController.java
@@ -0,0 +1,51 @@
+package com.dangbun.domain.notificationreceiver.controller;
+
+import com.dangbun.domain.notification.response.status.NotificationExceptionResponse;
+import com.dangbun.domain.notification.service.NotificationService;
+import com.dangbun.domain.notificationreceiver.dto.response.GetNotificationReceivedListResponse;
+import com.dangbun.domain.notificationreceiver.service.NotificationReceiverService;
+import com.dangbun.global.CheckPlaceMembership;
+import com.dangbun.global.docs.DocumentedApiErrors;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Validated
+@Tag(name = "Notification_Receiver", description = "NotificationReceiverController - 알림함(수신) 관련 API")
+@RestController
+@CheckPlaceMembership(placeIdParam = "placeId")
+@RequiredArgsConstructor
+public class NotificationReceiverController {
+
+    private final NotificationReceiverService notificationReceiverService;
+
+    @Operation(summary = "받은 알림 목록 조회 (무한스크롤)", description = "현재 로그인한 멤버가 받은 알림들을 무한스크롤 방식으로 조회합니다.")
+    @GetMapping("notifications/received")
+    @DocumentedApiErrors(
+            value = {NotificationExceptionResponse.class},
+            includes = {""}
+    )
+    public ResponseEntity<GetNotificationReceivedListResponse> getReceivedNotifications(
+            @RequestParam Long placeId,
+            @ParameterObject
+            @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC)
+            Pageable pageable
+    ) {
+        return ResponseEntity.ok(notificationReceiverService.getReceivedNotifications(pageable));
+    }
+
+
+}
