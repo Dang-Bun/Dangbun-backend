270770fc08c3647a9ab830d1251f944e08940de8
양지윤 <155087951+jiyun921@users.noreply.github.com>
2025-08-05 22:06:26 +0900

    [fix] import문 추가

--- src/main/java/com/dangbun/global/config/WebConfig.java
+++ src/main/java/com/dangbun/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.dangbun.global.config;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
