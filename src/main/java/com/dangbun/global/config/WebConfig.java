5ec15a5dd7b514f5af1a1b3d86ab009b3aaa0c61
양지윤 <155087951+jiyun921@users.noreply.github.com>
2025-08-05 22:03:18 +0900

    [feat] 프론트 연동 관련 cors 추가

--- src/main/java/com/dangbun/global/config/WebConfig.java
+++ src/main/java/com/dangbun/global/config/WebConfig.java
@@ -3,11 +3,16 @@ package com.dangbun.global.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/")
+                .allowedOrigins(
+                    "http://localhost:5173", // 프론트 로컬 주소 
+                    "https://dangbun-frontend-mu.vercel.app", // 프론트 배포 주소 
+                    "https://dangbun.o-r.kr" // 백엔드 배포 주소 
+                )
                 .allowedMethods("GET","POST","PATCH","DELETE", "PUT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
