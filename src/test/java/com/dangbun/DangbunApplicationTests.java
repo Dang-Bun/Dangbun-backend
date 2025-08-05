ffc0047c7da9c19ed2d78c884ee3e94bc4b51d40
김종혁 <jonnykim1o28@gmail.com>
2025-08-05 18:06:45 +0900

    [test]: S3Presigner MockBean 설정

--- src/test/java/com/dangbun/DangbunApplicationTests.java
+++ src/test/java/com/dangbun/DangbunApplicationTests.java
@@ -2,12 +2,17 @@ package com.dangbun;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class DangbunApplicationTests {
 
+	@MockBean
+	private S3Presigner s3Presigner;
+	
 	@Test
 	void contextLoads() {
 	}
