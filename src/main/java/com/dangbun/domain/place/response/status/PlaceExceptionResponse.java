6c4c8f2b10f27f2d176505f0e28d58ea19bb03ad
양지윤 <155087951+jiyun921@users.noreply.github.com>
2025-08-10 23:56:12 +0900

    [fix] PlaceExceptionResponse 오타 수정

--- src/main/java/com/dangbun/domain/place/response/status/PlaceExceptionResponse.java
+++ src/main/java/com/dangbun/domain/place/response/status/PlaceExceptionResponse.java
@@ -9,7 +9,7 @@ public enum PlaceExceptionResponse implements ResponseStatus {
     INVALID_INVITE_CODE(80000,"참여코드에 해당하는 플레이스가 없습니다."),
     ALREADY_INVITED(80001, "이미 초대된 사용자입니다."),
     INVALID_INFORMATION(80002, "플레이스가 요구한 맴버정보와 요청이 일치하지 않습니다."),
-    INVALID_TIME(80003,"시작시간이 종료시간보다 늦게 설정되었습니다.");
+    INVALID_TIME(80003,"시작시간이 종료시간보다 늦게 설정되었습니다."),
     INVALID_NAME(80004, "요청 값의 플레이스 이름이 현재 이름과 맞지 않습니다.");
 
     private final int code;
