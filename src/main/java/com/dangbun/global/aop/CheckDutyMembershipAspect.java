fe65da8c459ba7494346c80f2ae396aeed46522f
jiyun921 <ryn0921@naver.com>
2025-08-10 16:11:30 +0900

    [feat]: CheckDutyMembership 도입

---
+++ src/main/java/com/dangbun/global/aop/CheckDutyMembershipAspect.java
@@ -0,0 +1,68 @@
+package com.dangbun.global.aop;
+
+import com.dangbun.domain.duty.repository.DutyRepository;
+import com.dangbun.domain.member.entity.Member;
+import com.dangbun.domain.member.exception.custom.DutyAccessDeniedException;
+import com.dangbun.domain.memberduty.repository.MemberDutyRepository;
+import com.dangbun.domain.user.entity.CustomUserDetails;
+import com.dangbun.global.aop.support.AnnotationResolver;
+import com.dangbun.global.aop.support.RequestParamResolver;
+import com.dangbun.global.aop.support.SecuritySupport;
+import com.dangbun.global.context.MemberContext;
+import com.dangbun.global.exception.RequiredParamMissingException;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+import static com.dangbun.domain.member.response.status.MemberExceptionResponse.DUTY_ACCESS_DENIED;
+import static com.dangbun.global.response.status.BaseExceptionResponse.*;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class CheckDutyMembershipAspect {
+    private final MemberDutyRepository memberDutyRepository;
+    private final DutyRepository dutyRepository;
+
+    @Around("@within(com.dangbun.global.aop.CheckDutyMembership) || @annotation(com.dangbun.global.aop.CheckDutyMembership)")
+    public Object validate(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        CustomUserDetails userDetails = SecuritySupport.currentUserOrThrow();
+
+        CheckDutyMembership checkDutyMembership = AnnotationResolver.resolve(joinPoint, CheckDutyMembership.class);
+
+        String dutyIdParamName = checkDutyMembership.dutyIdParam();
+        Long dutyId = RequestParamResolver.resolveLong(joinPoint, dutyIdParamName);
+        if (dutyId == null) {
+            throw new RequiredParamMissingException(REQUIRED_PARAM_MISSING);
+        }
+
+        Member member = memberDutyRepository
+                .findMemberWithPlaceByDutyIdAndUserId(dutyId, userDetails.getUser().getUserId())
+                .orElseThrow(() -> new DutyAccessDeniedException(DUTY_ACCESS_DENIED));
+
+        String placeIdParam = checkDutyMembership.placeIdParam();
+        if (placeIdParam != null && !placeIdParam.isBlank()) {
+            Long placeId = RequestParamResolver.resolveLong(joinPoint, placeIdParam);
+            if (placeId == null){
+                throw new RequiredParamMissingException(REQUIRED_PARAM_MISSING);
+            }
+
+            boolean matchesPlace = dutyRepository.existsByDutyIdAndPlace_PlaceId(dutyId, placeId);
+            if (!matchesPlace) {
+                throw new DutyAccessDeniedException(DUTY_ACCESS_DENIED);
+            }
+        }
+
+        try {
+            MemberContext.set(member);
+            return joinPoint.proceed();
+        } finally {
+            MemberContext.clear();
+        }
+    }
+
+}
