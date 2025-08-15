a1ef907ca44c54279aff20191d9b256a7c05334c
김종혁 <jonnykim1o28@gmail.com>
2025-08-15 16:34:20 +0900

    [feat!]: Cleaning-add cleaningId in GetCleaningUnassingedResponse

--- src/main/java/com/dangbun/domain/cleaning/dto/response/GetCleaningUnassignedResponse.java
+++ src/main/java/com/dangbun/domain/cleaning/dto/response/GetCleaningUnassignedResponse.java
@@ -3,10 +3,13 @@ package com.dangbun.domain.cleaning.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record GetCleaningUnassignedResponse(
+        @Schema(description = "청소 Id", example = "1")
+        Long cleaningId,
+
         @Schema(description = "미지정 청소 이름", example = "바닥 닦기")
         String cleaningName
 ) {
-    public static GetCleaningUnassignedResponse of(String cleaningName) {
-        return new GetCleaningUnassignedResponse(cleaningName);
+    public static GetCleaningUnassignedResponse of(Long cleaningId, String cleaningName) {
+        return new GetCleaningUnassignedResponse(cleaningId, cleaningName);
     }
 }
