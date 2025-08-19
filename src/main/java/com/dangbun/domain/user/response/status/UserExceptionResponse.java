74c08c999ea06c7024acffca2871dc823f8a7887
김종혁 <jonnykim1o28@gmail.com>
2025-08-19 17:31:20 +0900

    [feat]: 인증코드 재전송 1분 제한 및 예외코드 추가

--- src/main/java/com/dangbun/domain/user/response/status/UserExceptionResponse.java
+++ src/main/java/com/dangbun/domain/user/response/status/UserExceptionResponse.java
@@ -10,7 +10,8 @@ public enum UserExceptionResponse implements ResponseStatus {
     INVALID_CERT_CODE(60002, "유효하지 않은 인증번호 입니다."),
     NO_SUCH_USER(60003, "해당 유저가 존재하지 않습니다."),
     INVALID_EMAIL(60004, "유효하지 않은 이메일입니다."),
-    DELETE_MEMBER(60005, "탈퇴한 회원입니다.");
+    DELETE_MEMBER(60005, "탈퇴한 회원입니다."),
+    AUTH_CODE_SENT(60006,"인증코드가 이미 전송되었습니다.");
 
     private final int code;
     private final String message;
