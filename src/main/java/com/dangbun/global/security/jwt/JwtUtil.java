2ad4f4da9166a31581be5734e3b7ae4eccf6a3fb
김종혁 <jonnykim1o28@gmail.com>
2025-09-29 21:58:52 +0900

    [refactor]: JwtUtil static 메서드만 보유하도록 리팩토링

--- src/main/java/com/dangbun/global/security/jwt/JwtUtil.java
+++ src/main/java/com/dangbun/global/security/jwt/JwtUtil.java
@@ -3,46 +3,33 @@ package com.dangbun.global.security.jwt;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.Base64;
+import java.util.Arrays;
 import java.util.Date;
 
+import static com.dangbun.global.security.jwt.TokenPrefix.REFRESH;
+
+@Slf4j
 @Component
 public class JwtUtil {
 
     @Value("${jwt.secret}")
-    private String SECRET;
+    private static String SECRET;
+    private static final Key KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
 
-    private Key key;
-    private JwtParser parser;
+    private JwtUtil() {}
 
-
-    @PostConstruct
-    void init() {
-        this.key = Keys.hmacShaKeyFor(SECRET.getBytes());
-
-        this.parser = Jwts.parserBuilder()
-                .setSigningKey(this.key)
-                .build();
-    }
-
-    private byte[] tryBase64Decode(String s) {
-        try {
-            return Base64.getDecoder().decode(s);
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-    }
-
-    public String parseAccessToken(String authorizationHeader) {
+    public static String parseAccessToken(String authorizationHeader) {
         if (authorizationHeader == null || authorizationHeader.isBlank()) {
             throw new IllegalArgumentException("Authorization header is empty");
         }
-        final String prefix = "Bearer ";
+        final String prefix = TokenPrefix.BEARER.getName();
         if (!authorizationHeader.startsWith(prefix)) {
             throw new IllegalArgumentException("Authorization header must start with 'Bearer '");
         }
@@ -53,25 +40,60 @@ public class JwtUtil {
         return token;
     }
 
-    public Claims parseClaims(String token) {
-        return parser
-                .parseClaimsJws(token)
+    public static String getRefreshToken(HttpServletRequest request) {
+        try {
+            if (request.getCookies() != null) {
+                return Arrays.stream(request.getCookies())
+                        .filter(cookie -> REFRESH.getName().equals(cookie.getName()))
+                        .map(Cookie::getValue)
+                        .findFirst()
+                        .orElse(null);
+            }
+        } catch (Exception e) {
+            log.error("refreshToken error", e);
+        }
+        return null;
+    }
+
+    public static Claims parseToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(KEY)
+                .build()
+                .parseClaimsJws(stripBearer(token))
                 .getBody();
     }
 
-    public Date getExpiration(String token) {
-        return parseClaims(token).getExpiration();
-    }
-
-    public String validateAndGetUserId(String token) {
+    public static boolean validateToken(String token) {
+        if (token == null || token.isBlank()) return false;
         try {
-            return parseClaims(token).getSubject();
-        } catch (ExpiredJwtException e) {
-            throw e;
-        } catch (UnsupportedJwtException | MalformedJwtException e) {
-            throw e;
+            parseToken(token);
+            return true;
+        } catch (ExpiredJwtException e){
+            return false;
+        } catch (UnsupportedJwtException | MalformedJwtException e){
+            return false;
+        } catch (io.jsonwebtoken.security.SecurityException e){
+            return false;
+        } catch (IllegalArgumentException e){
+            return false;
         }
+
     }
 
+    public Date getExpiration(String token) {
+        return parseToken(token).getExpiration();
+    }
+
+
+    public static String getSubject(String token) {
+        return parseToken(token).getSubject();
+    }
+
+    public static String stripBearer(String token) {
+        if (token != null && token.startsWith(TokenPrefix.BEARER.getName())) {
+            return token.substring(7);
+        }
+        return token;
+    }
 
 }
